9e6f208fdb132153e536e9eaf8bc118a9efa364b
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2023-11-24 12:43:41 -0500

    initial commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+const headerElement = document.getElementById("headling");
+const adviceElement = document.getElementById("advice");
+const api = "https://api.adviceslip.com/advice";
+const apiEndPoint = async () => {
+	try {
+		const apiResponse = await fetch(api);
+		if (apiResponse.ok) {
+			const jsonResponse = await apiResponse.json();
+			headerElement.innerHTML = `ADVICE #${jsonResponse.slip.id}`;
+			adviceElement.innerHTML = `${jsonResponse.slip.advice}`;
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+apiEndPoint();
