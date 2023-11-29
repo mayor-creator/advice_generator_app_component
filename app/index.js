9e8bbd8a83427de186861c4eba4a0e5d0ee51f12
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2023-11-29 09:59:18 -0500

    fix quotation mark issue on the adviceElement

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ const apiEndPoint = async () => {
 		if (apiResponse.ok) {
 			const jsonResponse = await apiResponse.json();
 			headerElement.innerHTML = `ADVICE #${jsonResponse.slip.id}`;
-			adviceElement.innerHTML = `${jsonResponse.slip.advice}`;
+			adviceElement.innerHTML = `\"${jsonResponse.slip.advice}\"`;
 		}
 	} catch (error) {
 		console.log(error);
